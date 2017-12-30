d778550da9e37d7f18d4762f54405d16ed05f2c3
Sheila Owens <ssowens711@gmail.com>
2017-12-30 16:23:37 -0600

    udpated settings2

---
+++ app/src/main/java/com/ssowens/android/popularmovies/SettingsFragment.java
@@ -0,0 +1,43 @@
+package com.ssowens.android.popularmovies;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import static com.ssowens.android.popularmovies.SettingsActivity.PREF_MOVIES_KEY;
+
+public class SettingsFragment extends PreferenceFragment implements
+        SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if (key.equals(PREF_MOVIES_KEY)) {
+            Preference connectionPref = findPreference(key);
+            // Set summary to be the user-description for the selected value
+            connectionPref.setSummary(sharedPreferences.getString(key, ""));
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        /* Unregister the preference change listener */
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        /* Register the preference change listener */
+        getPreferenceScreen().getSharedPreferences()
+                .registerOnSharedPreferenceChangeListener(this);
+    }
+}
