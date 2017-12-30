ea4323614742a51a61b1e5bad6b60bae30c9f817
Sheila Owens <ssowens711@gmail.com>
2017-12-30 16:16:29 -0600

    udpated settings

--- app/src/main/java/com/ssowens/android/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/ssowens/android/popularmovies/SettingsActivity.java
@@ -1,19 +1,16 @@
 package com.ssowens.android.popularmovies;
 
 import android.os.Bundle;
-import android.preference.ListPreference;
-import android.preference.Preference;
 import android.preference.PreferenceActivity;
-import android.preference.PreferenceManager;
 import android.util.Log;
-import android.widget.LinearLayout;
+import android.view.MenuItem;
 
 /**
  * Created by Sheila Owens on 12/18/16.
  */
 
-public class SettingsActivity extends PreferenceActivity
-        implements Preference.OnPreferenceChangeListener {
+public class SettingsActivity extends PreferenceActivity {
+
 
     public final static String TAG = "SettingsActivity";
     public static final String PREF_MOVIES_KEY = "pref_movies_key";
@@ -23,56 +20,32 @@ public class SettingsActivity extends PreferenceActivity
         Log.i(TAG, "onCreate()");
         super.onCreate(savedInstanceState);
 
-        // Set the default values when this method has never been called.
-        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        this.setContentView(R.layout.popular_movies_settings);
+//        this.getActionBar().setDisplayHomeAsUpEnabled(true);
 
-        // Add 'general' preferences, defined in the XML file
-        addPreferencesFromResource(R.xml.preferences);
-
-        LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent()
-                .getParent()
-                .getParent();
-
-
-        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
-        // updated when the preference changes.
-        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_movies_key)));
-    }
-
-    /**
-     * Attaches a listener so the summary is always updated with the preference value.
-     * Also fires the listener once, to initialize the summary (so it shows up before the value
-     * is changed.)
-     */
-    private void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
-        preference.setOnPreferenceChangeListener(this);
-
-        // Trigger the listener immediately with the preference's
-        // current value.
-        onPreferenceChange(preference,
-                PreferenceManager
-                        .getDefaultSharedPreferences(preference.getContext())
-                        .getString(preference.getKey(), ""));
+//        // Set the default values when this method has never been called.
+//        PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+//
+//        // Add 'general' preferences, defined in the XML file
+//        addPreferencesFromResource(R.xml.preferences);
+//
+//        LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent()
+//                .getParent()
+//                .getParent();
+//
+//
+//        // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
+//        // updated when the preference changes.
+//        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_movies_key)));
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        String stringValue = newValue.toString();
-
-        if (preference instanceof ListPreference) {
-            // For list preferences, look up the correct display value in
-            // the preference's 'entries' list (since they have separate labels/values).
-            ListPreference listPreference = (ListPreference) preference;
-            int prefIndex = listPreference.findIndexOfValue(stringValue);
-            if (prefIndex >= 0) {
-                preference.setSummary(listPreference.getEntries()[prefIndex]);
-            }
-        } else {
-            // For other preferences, set the summary to the value's simple string representation.
-            preference.setSummary(stringValue);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            onBackPressed();
         }
-        return true;
+        return super.onOptionsItemSelected(item);
     }
 
 }
